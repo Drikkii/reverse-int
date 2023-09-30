235180b146fafff142f654055942b0e62715ab7b
@Drikkii <chevpavel@gmail.com>
2023-09-30 15:40:51 +0300

    dw

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const numberArray = Array.from(String(n));
+    const reversedArray = numberArray.reverse();
+    const reversedNumber = parseInt(reversedArray.join(""));
+    return reversedNumber;
+};
